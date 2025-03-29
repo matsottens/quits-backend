ef8fb50595fa98dd274d352998831868993cee05
mats <mats_ottens@hotmail.com>
2025-03-29 16:19:20 +0100

    Refactor calendar components to standardize icon prop names and improve class handling

--- src/pages/signup-page/components/ui/calendar.tsx
+++ src/pages/signup-page/components/ui/calendar.tsx
@@ -53,8 +53,8 @@ function Calendar({
         ...classNames,
       }}
       components={{
-        CaptionLeftIcon: () => <ChevronLeft className="h-4 w-4" />,
-        CaptionRightIcon: () => <ChevronRight className="h-4 w-4" />,
+        IconLeft: () => <ChevronLeft className="h-4 w-4" />,
+        IconRight: () => <ChevronRight className="h-4 w-4" />,
       } as CustomComponents}
       {...props}
     />
